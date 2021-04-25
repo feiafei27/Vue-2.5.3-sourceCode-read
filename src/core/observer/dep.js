1495e515f20fa396e731478189fd38e03d4032ff
fei_fei27 <2543736927@qq.com>
2021-04-25 19:25:58 +0800

    开始阅读 dep.js

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -9,7 +9,14 @@ let uid = 0
  * A dep is an observable that can have multiple
  * directives subscribing to it.
  */
+/**
+ * 用于收集依赖的类
+ * 所谓的依赖就是一个个的 Watcher 实例
+ */
 export default class Dep {
+  // target 是一个静态的变量，可以在程序的任何地方使用 Dep.target 访问到这个变量
+  // 他的作用是：在收集依赖的时候，将要收集的 Watcher 实例保存在这里，然后调用 dep.depend() 函数
+  // 将这个 Watcher 实例保存到 Dep 的 subs 数组中
   static target: ?Watcher;
   id: number;
   subs: Array<Watcher>;
