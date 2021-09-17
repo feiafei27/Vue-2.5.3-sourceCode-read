65b63e707e4fc4de0f5fb8853319b89f903f5c51
fei_fei27 <2543736927@qq.com>
2021-09-17 18:28:21 +0800

    研究过滤器

--- test/vue-analysis/main.js
+++ test/vue-analysis/main.js
@@ -1,52 +1,11 @@
-// 父组件代码字符串
 with(this){
   return _c(
     'div',
-    {staticClass:"app_container"},
+    {attrs:{"id":"app"}},
     [
-      _c('h1',[_v("我是APP")]),
-      _v(" "),
-      _c(
-        'hello-world',
-        {
-          scopedSlots:_u(
-            [
-              {
-                key:"default",
-                // slotProps = {
-                //   user: {
-                //     name: 'tom'
-                //   }
-                // }
-                fn:function(slotProps){
-                  return [
-                    _c(
-                      'h3',
-                      [_v(_s(slotProps.user.name))]
-                    )
-                  ]
-                }
-              }
-            ]
-          )
-        }
-      )
-    ],
-    1
-  )
-}
-
-
-// 子组件代码字符串
-with(this){
-  return _c(
-    'div',
-    {staticClass:"hello_world"},
-    [
-      _c('h2',[_v("我是 HelloWorld 组件")]),
-      _v(" "),
-      _t("default",null,{"user":user})
-    ],
-    2
+      _c('h3',[_v(_s(_f("capitalize")(message)))]),
+      _c('h3',[_v(_s(_f("suffix")(message,'!')))]),
+      _c('h3',[_v(_s(_f("prefix")(_f("capitalize")(message))))])
+    ]
   )
 }
